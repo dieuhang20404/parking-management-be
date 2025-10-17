cdd3eea1ae6480d442e082a92f344322bf46ed09
Le-Thi-Truc-Ha <hale071204@gmail.com>
2025-10-18 00:12:09 +0700

    Remove file isnt used

--- src/routes/app.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as controller from "../controllers/app.controller";
-
-let appRoute = express.Router();
-
-appRoute.get("/test-api", controller.testApiController);
-
-export default appRoute;
--- src/routes/web.route.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "express";
-import appRoute from "./app.route";
-
-const initWebRoute = (app: Application) => {
-    app.use("/", appRoute);
-}
-
-export default initWebRoute;
