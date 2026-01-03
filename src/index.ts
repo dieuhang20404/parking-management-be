df9e82f442065803e4a1c8526203dfbda00879b2
Le-Thi-Truc-Ha <hale071204@gmail.com>
2026-01-04 03:50:56 +0700

    Test socket

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import initWebRoute from "./routes/web.route";
 import { Server } from "socket.io";
 import http from "http";
 import initSocketRoute from "./socket/socket.route";
+import { setSocketIO } from "./configs/socket";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const app: Application = express();
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server, {cors: socketCors});
+setSocketIO(io);
 
 expressCors(app);
 
